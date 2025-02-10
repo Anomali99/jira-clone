bb8467d260e068b55f41034c1f4d3b3c3358ba82
Anomali99 <fatiqnur@gmail.com>
2025-02-10 12:17:38 +0700

    fix error

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -3,18 +3,15 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
-  const origin = process.env.NEXT_PUBLIC_APP_URL;
-
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${origin}/oauth`,
-    `${origin}/sign-up`
+    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
   );
 
   return redirect(redirectUrl);
@@ -23,12 +20,10 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
   const { account } = await createAdminClient();
 
-  const origin = process.env.NEXT_PUBLIC_APP_URL;
-
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${origin}/oauth`,
-    `${origin}/sign-up`
+    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
   );
 
   return redirect(redirectUrl);
